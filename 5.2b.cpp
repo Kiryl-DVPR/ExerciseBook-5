b889d053e75e5a5280bcd64162bcde05fdaa0815
Кирилл Сотов <katana96@mail.ru>
2022-04-04 22:51:18 +0300

    Exercise 5.2c

--- 5.2b.cpp
+++ 5.2b.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<iomanip>
+#include<cmath>
 using namespace std;
 
 int main()
@@ -9,4 +10,7 @@ int main()
     cout << setw(15) << left << fixed << setprecision(1) << number;
     cout << setw(15) << left << fixed << setprecision(2) << number;
     cout << setw(15) << left << fixed << setprecision(3) << number;
+
+
+    cout << endl << setw(10) << right <<fixed << setprecision(2) << pow(2.5,3);
 }
