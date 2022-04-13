813c09f1d274f01189b10ef6a0be85ab27d757be
Кирилл Сотов <katana96@mail.ru>
2022-04-13 17:30:05 +0300

    Exercise 4.5c.d finish

--- 5.2b.cpp
+++ 5.2b.cpp
@@ -12,5 +12,39 @@ int main()
     cout << setw(15) << left << fixed << setprecision(3) << number;
 
 
-    cout << endl << setw(10) << right <<fixed << setprecision(2) << pow(2.5,3);
+    cout << endl << setw(10) << right <<fixed << setprecision(2) << pow(2.5,3) << endl;
+
+    int x=1;
+
+    while (x<=20)
+
+    {   
+      
+      if (x%5==0)
+      {
+          cout << x << endl;
+      } else
+      {
+          cout << x << "\t";
+      }
+      
+
+        x++;
+    }
+
+    
+    for ( int i = 1; i <= 20; i++)
+    {
+        
+        if (i%5==0)
+      {
+          cout << i << endl;
+      } else
+      {
+          cout << i << "\t";
+      }
+
+    }
+    
+    
 }
