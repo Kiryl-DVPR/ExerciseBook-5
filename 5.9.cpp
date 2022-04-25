b560e77d5706f45237cab4158f425774788a02dd
Kiryl <katana96@mail.ru>
2022-04-25 17:19:03 +0300

    Exercise 5.9

---
+++ 5.9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int prod=1;
+
+    for (int i = 1; i<=15; i+=2)
+    {   
+        prod = prod * i;
+        
+    }
+
+    cout << prod;
+    
+}
