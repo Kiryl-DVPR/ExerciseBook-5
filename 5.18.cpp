31727bf06d8793f66e4efb3628b81565eda5a0c8
Kiryl <katana96@mail.ru>
2022-06-04 16:17:47 +0300

    Exercise 5.19

--- 5.18.cpp
+++ 5.18.cpp
@@ -33,7 +33,6 @@ return 0;
 
 }
 
-
 int main()
 { 
         for (int i = 0; i <= 256; i++)
--- 5.19.cpp
+++ 5.19.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+    double s=1, p;
     
+    cout << "P\t" << "Row Euler" << endl;
+        for (int i = 2; i < 100; i++)
+    {
+        s=s+(1/(pow(i,2)));
+        p=pow((s*6),0.5);
+        cout << p << "\t" << i << endl;
+    }
+     
 }
