4abb3ab9ce7f82bbc11b53911bf80666677aa0dd
Kiryl <katana96@mail.ru>
2022-06-06 11:12:54 +0300

    Finish Exercise 5.19

--- 5.19.cpp
+++ 5.19.cpp
@@ -5,14 +5,29 @@ using namespace std;
 
 int main()
 {
-    double s=1, p;
+    double s, p, w=0;
     
     cout << "P\t" << "Row Euler" << endl;
-        for (int i = 2; i < 100; i++)
+        /*for (int i = 2; i < 500000; i++)
     {
         s=s+(1/(pow(i,2)));
         p=pow((s*6),0.5);
-        cout << p << "\t" << i << endl;
+        if (p==3.14159)
+        {
+            cout << p << "\t" << i << endl;
+        }
+          
+    }*/
+
+    for (double i = 1; i < 100000; i=i+4)
+    {
+        
+        p=p+((4/i));
+        w++;
+        cout << p << "\t" << w << endl;
+        p=p-(4/(i+2));
+        w++;
+        cout << p << "\t" << w << endl;
     }
-     
+      
 }
