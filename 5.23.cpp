5e0bd71cb8ed714594563ea58528ec91c2d7d1f2
Kiryl <katana96@mail.ru>
2022-06-13 17:14:40 +0300

    Exercise 5.23

--- 5.23.cpp
+++ 5.23.cpp
@@ -6,16 +6,41 @@ using namespace std;
 int main()
 {
     
-    for (int x = 1; x <=5; x++)
-    {   
+    for (int b = 1, c=4; b <=9; b=b+2, c=c-1)
+    {
+        for (int a = 1; a <= c; a++)
+            {
+                cout << " ";
+            }
 
-        for (int i = 1; i <=x; i++)
-        {
+        for (int i = 1; i <= b ; i++)
+        {               
             cout << "*" ;
         }
 
-    cout << endl;
-        
+        cout << endl;
+            
     }
-    
+
+    int b = 7, c=1;
+
+    while (c!=5)
+    {
+        for (int a = 1; a <= c; a++)
+            {
+                cout << " ";
+            }
+            
+        for (int i = 1; i <= b ; i++)
+        {               
+            cout << "*" ;
+        }
+
+        cout << endl;
+
+        b=b-2;
+        c=c+1;
+            
+    }
+           
 }
