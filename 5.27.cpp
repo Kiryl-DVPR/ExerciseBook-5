450f26b42cfaf44ffb3a2bd76d09a37261c44d6b
Kiryl <katana96@mail.ru>
2022-06-15 09:42:02 +0300

    Exercise 4.27

---
+++ 5.27.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+
+int main ()
+{
+    int count;
+
+    for (count = 1; count <= 10; count++)
+    {
+        
+        if ( count == 5)
+        {
+        }
+        else
+        {
+            cout << count << " ";
+        }
+        
+    }
+
+    cout << "\nUsed continue to skip printing 5" << endl; 
+}
